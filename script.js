b4fcc264929ac3a7b990e4cac87d210d1e0c6860
Vaishnavi <140810920+Vaishnavi822@users.noreply.github.com>
2023-07-28 18:26:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+  
+  function openTab(tabName) {
+    var i, x;
+    x = document.getElementsByClassName("containerTab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+  }
